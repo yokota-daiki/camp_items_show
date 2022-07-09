6ae07210968ee5aa609307a27a8e54757802e6dc
yokota-daiki <ribrate78@gmail.com>
2022-07-10 00:44:27 +0900

    Add:i18nによる日本語化の対応

--- Gemfile
+++ Gemfile
@@ -31,6 +31,7 @@ gem 'bootsnap', '>= 1.4.4', require: false
 gem 'net-imap'
 gem 'net-pop'
 gem 'net-smtp'
+gem 'rails-i18n'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- config/application.rb
+++ config/application.rb
@@ -23,6 +23,11 @@ module CampItemsShow
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
+    config.i18n.default_locale = :ja
+
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
 
     # Configuration for the application, engines, and railties goes here.
     #
