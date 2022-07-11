616658780d44cce15852ab386a6877d5e82e7014
yokota-daiki <ribrate78@gmail.com>
2022-07-12 01:25:12 +0900

    Add:フラッシュメッセージの作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   skip_before_action :require_login, only: %i[new create]
-  
+
   def new
     @user = User.new
   end
@@ -8,7 +8,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      redirect_to root_path
+      redirect_to root_path, success: t('.success')
     else
       render :new
     end
