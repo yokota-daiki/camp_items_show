ab481618906d753fa46b631762058890672306c4
yokota-daiki <ribrate78@gmail.com>
2022-07-11 23:29:27 +0900

    Add:簡易的なtopページの作成

---
+++ app/controllers/top_page_controller.rb
@@ -0,0 +1,5 @@
+class TopPageController < ApplicationController
+
+  def top
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
+  root "top_page#top"
   resources :users, only: %i[new create update]
 end
