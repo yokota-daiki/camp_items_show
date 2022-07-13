faf131dc560eb2d00dc9ebe7118416b7576b5f28
yokota-daiki <ribrate78@gmail.com>
2022-07-13 23:29:59 +0900

    Fix:topページのコントローラの名称を変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root "static_page#top"
   resources :users, only: %i[new create update]
-  get "login", to: "user_sessions#new"
-  post "login", to: "user_sessions#create"
+  get "login",     to: "user_sessions#new"
+  post "login",    to: "user_sessions#create"
+  delete "logout", to: "user_sessions#destroy"
 end
