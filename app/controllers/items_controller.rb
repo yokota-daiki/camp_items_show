68844b337426490bac4e141f2ea0d1db598299e9
yokota-daiki <ribrate78@gmail.com>
2022-07-23 18:39:41 +0900

    Add:楽天商品検索APIを使用して検索機能を実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,11 +1,35 @@
 class ItemsController < ApplicationController
-
+  
   def search
+    @keyword = params[:keyword]
+    if @keyword.present?
+      @items = RakutenWebService::Ichiba::Item.search(keyword: @keyword)
+      #results.each do |result|
+        #item = Item.new(read(result))
+        #@items << item
+      #end
+    end
   end
 
   def index
   end
 
   def new
+  end
+
+  def create
+  end
+
+  private
+
+  def read(result)
+    name = result["itemName"]
+    url = result["itemUrl"]
+    image_url = result["smallImageUrls"]
+    {
+      name: "name",
+      url: "url",
+      image_url: "image_url"
+    }
   end
 end
--- config/initializers/rakuten.rb
+++ config/initializers/rakuten.rb
@@ -1,3 +1,3 @@
-RakutenWebService.configuration do |c|
+RakutenWebService.configure do |c|
   c.application_id = '1053682478810787162'
 end
