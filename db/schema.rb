8481f5cfbc66adfc33a27323a54d449c1a0dcbbd
yokota-daiki <ribrate78@gmail.com>
2022-07-23 15:04:12 +0900

    Add:Itemテーブルにカラムを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_07_22_144026) do
+ActiveRecord::Schema.define(version: 2022_07_23_060136) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -57,6 +57,8 @@ ActiveRecord::Schema.define(version: 2022_07_22_144026) do
     t.string "maker"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "image_url"
+    t.integer "category"
   end
 
   create_table "posts", force: :cascade do |t|
