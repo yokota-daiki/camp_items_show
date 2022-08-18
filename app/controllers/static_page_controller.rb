5d082aac7dca4dba3fea9d6c91025a980877cb1c
yokota-daiki <ribrate78@gmail.com>
2022-08-18 12:39:19 +0900

    Add:topページに表示される投稿をランダム表示に変更

--- app/controllers/static_page_controller.rb
+++ app/controllers/static_page_controller.rb
@@ -1,6 +1,7 @@
 class StaticPageController < ApplicationController
   skip_before_action :require_login, only: %i[top]
   def top
-    @posts = Post.where(public: true).with_attached_images
+    all_posts = Post.where(public: true).with_attached_images
+    @posts = all_posts.sample(6)
   end
 end
