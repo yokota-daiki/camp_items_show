a25c967f8426b55615d22d43e79e872228494b79
yokota-daiki <ribrate78@gmail.com>
2022-08-18 17:45:21 +0900

    style:キャンプ投稿一覧ページのモーダル表示のデザイン変更

--- app/controllers/static_page_controller.rb
+++ app/controllers/static_page_controller.rb
@@ -1,7 +1,6 @@
 class StaticPageController < ApplicationController
   skip_before_action :require_login, only: %i[top]
   def top
-    all_posts = Post.where(public: true).with_attached_images
-    @posts = all_posts.sample(6)
+    @posts = Post.where(public: true).with_attached_images.sample(6)
   end
 end
