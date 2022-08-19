43097c02410f815641ff8851b04db144c264d169
yokota-daiki <ribrate78@gmail.com>
2022-08-20 02:21:51 +0900

    add:TOPページのキャンプ画像のリロード機能の追加

--- app/controllers/static_page_controller.rb
+++ app/controllers/static_page_controller.rb
@@ -3,4 +3,8 @@ class StaticPageController < ApplicationController
   def top
     @posts = Post.where(public: true).with_attached_images.sample(6)
   end
+
+  def reload
+    @posts = Post.where(public: true).with_attached_images.sample(6)
+  end
 end
