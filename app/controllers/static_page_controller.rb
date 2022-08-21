8fe1f41810681370942a6b29759f3d57876b2418
yokota-daiki <ribrate78@gmail.com>
2022-08-21 23:00:56 +0900

    fix:未ログイン時でもtopページの投稿を更新できるよう修正

--- app/controllers/static_page_controller.rb
+++ app/controllers/static_page_controller.rb
@@ -1,5 +1,5 @@
 class StaticPageController < ApplicationController
-  skip_before_action :require_login, only: %i[top]
+  skip_before_action :require_login, only: %i[top reload]
   def top
     @posts = Post.where(public: true).with_attached_images.sample(6)
   end
