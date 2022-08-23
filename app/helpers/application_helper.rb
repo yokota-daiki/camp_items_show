33542851b3872b62f6cb589ed3953bb3b8132a05
yokota-daiki <ribrate78@gmail.com>
2022-08-23 23:55:32 +0900

    Add:タイトルを動的に変化させるヘルパーを記述

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  def page_title(page_titlea="", admin: false)
+    base_title = 
+      if admin
+        '管理画面'
+      else
+        'CAMPERs'
+      end
+    page_title.empty? ? base_title : page_title + "|" + base_title
+  end
 end
