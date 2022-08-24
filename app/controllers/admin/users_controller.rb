cdb687524c184b35575f106c044fb40555a9b07c
yokota-daiki <ribrate78@gmail.com>
2022-08-24 17:24:05 +0900

    Add:ユーザー一覧ページを作成しユーザーの削除機能の追加

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,10 +1,21 @@
 class Admin::UsersController < Admin::BaseController
+  before_action :set_user, only: %i[destroy]
+
   def index
+    @users = User.all
   end
 
   def show
   end
 
   def destroy
+    @user.destroy!
+    redirect_to admin_users_path, success: t('success')
+  end
+
+  private
+  
+  def set_user
+    @user = User.find(params[:id])
   end
 end
