5f68ab1623a9912ee2fd72e52182e2230b773932
yokota-daiki <ribrate78@gmail.com>
2022-08-26 00:16:31 +0900

    Add:メニューをアクティブ化

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,8 @@ module ApplicationHelper
     base_title = 'CAMPERs'
     page_title.empty? ? base_title : page_title + "|" + base_title
   end
+
+  def active_if(path)
+    path == controller_path ? 'active' : ''
+  end
 end
