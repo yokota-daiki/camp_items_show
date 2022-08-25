b283cd40aaf37105eba90103516bcd82bbbf6843
yokota-daiki <ribrate78@gmail.com>
2022-08-25 13:47:22 +0900

    Add:ページネーションを投稿一覧ページに設置

--- app/controllers/admin/posts_controller.rb
+++ app/controllers/admin/posts_controller.rb
@@ -2,7 +2,7 @@ class Admin::PostsController < Admin::BaseController
   before_action :set_post, only: %i[destroy]
 
   def index
-    @posts = Post.includes(:user).with_attached_images.all
+    @posts = Post.includes(:user).with_attached_images.order(created_at: :desc).page(params[:page])
   end
 
   def show
