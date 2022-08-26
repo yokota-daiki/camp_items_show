038b169653d555ce74948a83177ca1526c3979d8
yokota-daiki <ribrate78@gmail.com>
2022-08-26 18:34:56 +0900

    Add:not_authenticatedメソッドを改めて定義

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -8,7 +8,7 @@ class UserSessionsController < ApplicationController
   def create
     @user = login(params[:email], params[:password])
     if @user
-      redirect_back_or_to root_path, success: t('.success')
+      redirect_to root_path, success: t('.success')
     else
       flash.now[:danger] = t('.fail')
       render :new 
