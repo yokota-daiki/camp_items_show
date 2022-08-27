03f8176a9caeb50d61b6dd54ca489832fd12765b
yokota-daiki <ribrate78@gmail.com>
2022-08-27 20:27:27 +0900

    test:postのテストデータ作成

--- spec/factories/posts.rb
+++ spec/factories/posts.rb
@@ -1,9 +1,19 @@
 FactoryBot.define do
   factory :post do
-    user { nil }
-    camp_field { "MyString" }
-    comment { "MyString" }
-    public { false }
-    camped_date { "2022-07-15" }
+    camp_field { "test_camp" }
+    comment { "test_comment" }
+    public { 'false' }
+    camped_date { "002022-07-15" }
+    association :user
+  end
+
+  trait :public do
+    camp_field { "public" }
+    public { 'true' }
+  end
+
+  trait :private do
+    camp_field { "private" }
+    public { 'false' }
   end
 end
