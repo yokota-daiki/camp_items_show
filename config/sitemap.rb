6f5e36ff0fe77e0702d0f54ac726428e89905793
yokota-daiki <ribrate78@gmail.com>
2022-09-07 15:28:18 +0900

    Add:sitemapの編集

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -1,9 +1,12 @@
 # Set the host name for URL creation
 SitemapGenerator::Sitemap.default_host = "https://www.campers-gear.net/"
-SitemapGenerator::Sitemap.sitemaps_host = "https://campers-gearI.s3.amazonaws.com/"
-SitemapGenerator::Sitemap.public_path = 'public/'
-SitemapGenerator::Sitemap.sitemaps_path = 'sitemaps/'
-SitemapGenerator::Sitemap.adapter = SitemapGenerator::WaveAdapter.new
+SitemapGenerator::Sitemap.sitemaps_host = "https://s3-ap-northeast-1.amazonaws.com/#{ENV['S3_BUCKET_NAME']}"SitemapGenerator::Sitemap.public_path = 'public/'
+SitemapGenerator::Sitemap.adapter = SitemapGenerator::AwsSdkAdapter.new(
+  ENV['S3_BUCKET_NAME'],
+  aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+  aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
+  aws_region: 'ap-northeast-1',
+)
 
 SitemapGenerator::Sitemap.create do
   # Put links creation logic here.
