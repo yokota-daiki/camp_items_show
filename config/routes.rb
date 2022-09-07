12ff369aa8be492cc0be20e2b34eae29f61856a7
yokota-daiki <ribrate78@gmail.com>
2022-09-07 19:36:56 +0900

    Add

--- config/routes.rb
+++ config/routes.rb
@@ -24,5 +24,5 @@ Rails.application.routes.draw do
   post "login",        to: "user_sessions#create"
   delete "logout",     to: "user_sessions#destroy"
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
-  get '/sitemap', to: redirect("https://s3-ap-northeast-1.amazonaws.com/#{ENV['S3_BUCKET_NAME']}/sitemaps/sitemap.xml.gz")
+  get '/sitemap', to: redirect("https://s3-ap-northeast-1.amazonaws.com/#{ENV['S3_BUCKET_NAME']}/sitemap.xml.gz")
 end
