72cc9c2da598b71d5006267b5136366c2004a65a
yokota-daiki <ribrate78@gmail.com>
2022-09-18 14:18:30 +0900

    refact:SearchItemsコントローラのsearchアクション内のeachメソッドをmapメソッドに変更

--- app/controllers/search_items_controller.rb
+++ app/controllers/search_items_controller.rb
@@ -1,41 +1,35 @@
 class SearchItemsController < ApplicationController
   def search
     @keyword = params[:keyword]
-    @items = []
     if @keyword.present?
       @results = RakutenWebService::Ichiba::Product.search(keyword: @keyword, genreId: 101_975) # 楽天価格ナビ製品検索で渡されたkeywordを元に検索
 
-      childe_categories = []
-      @results.each do |result|
-        childe_categories << result['genreId'] # 検索結果のアイテムのジャンルIDを配列に挿入
+      childe_categories = @results.map do |result| #検索結果のアイテムのジャンルIDを配列を作成
+        result['genreId'] 
       end
 
-      parent_categories = []
-      childe_categories.each do |c|
-        parent_categories << RakutenWebService::Ichiba::Genre[c] # アイテム検索で取得したアイテムのジャンルIDをそれぞれ楽天ジャンルAPI検索にかけて，配列に挿入
+      parent_categories = childe_categories.map do |childe_category| #アイテム検索で取得したアイテムのジャンルIDをそれぞれ楽天ジャンルAPI検索にかけて，配列を作成
+        RakutenWebService::Ichiba::Genre[childe_category] 
       end
 
-      categories = []
-      parent_categories.each do |p|
-        categories << if p.parents[2].present?
-                        p.parents[2]['genreName'] # 親のジャンルを取得
-                      end
+      categories = parent_categories.map do |parent_category|
+        parent_category.parents[2]['genreName'] if parent_category.parents[2].present? # 親のジャンルを取得
       end
-      @results.zip(categories) do |result, c|
-        item = Item.new(read(result, c))
-        @items << item
+
+      @items = @results.zip(categories) do |result, category|
+        item = Item.new(read(result, category))
       end
     end
   end
 
   private
 
-  def read(result, c)
+  def read(result, category)
     name = result['productName']
     url = result['productUrlPC']
     image_url = result['mediumImageUrl']
     maker = result['makerName']
-    category = c
+    category = category
     { name:, url:, image_url:, maker:, category: }
   end
 end
