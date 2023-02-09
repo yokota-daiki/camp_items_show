49b36968001799f08c34007e32865ee5b62173ac
yokota-daiki <ribrate78@gmail.com>
2023-02-09 15:58:30 +0900

    Test: postモデルのmodel specを作成

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,5 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let!(:user) { create(:user) }
+  it '全ての属性が存在していたら有効である' do
+    post = Post.new(
+      user: user,
+      camp_field: "test_camp_field",
+      comment: "test_comment",
+      camped_date: "002022-07-15"
+    )
+
+    expect(post).to be_valid
+  end
+
+  it 'キャンプ場とキャンプ日時が存在していないと無効である' do
+    post = Post.new(
+      user: user,
+      camp_field: nil,
+      comment: "test_comment",
+      camped_date: nil
+    )
+
+    post.valid?
+    expect(post.errors.full_messages).to include("訪れたキャンプ場を入力してください", "日付を入力してください")
+  end
 end
