cc654fa4c87807de9a612bb9364591607c3c5436
yokota-daiki <ribrate78@gmail.com>
2023-02-10 18:46:51 +0900

    Test:checklistモデルの model specを作成

--- spec/models/checklist_spec.rb
+++ spec/models/checklist_spec.rb
@@ -1,5 +1,32 @@
 require 'rails_helper'
 
 RSpec.describe Checklist, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let!(:user) { create(:user) }
+  it '全ての属性が存在していたら有効である' do
+    checklist = Checklist.new(
+      name: "test_checklist",
+      user: user
+    )
+
+    expect(checklist).to be_valid
+  end
+
+  it 'チェックリスト名が存在していないと無効である' do
+    checklist = Checklist.new(
+      name: nil,
+      user: user
+    )
+
+    checklist.valid?
+    expect(checklist.errors.full_messages).to include("チェックリスト名を入力してください")
+  end
+
+  it 'userが存在していないと無効である' do
+    checklist = Checklist.new(
+      name: "test_checklist",
+      user: nil
+    )
+
+    expect(checklist).to be_invalid
+  end
 end
