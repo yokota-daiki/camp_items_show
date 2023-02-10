3899fbf8c9e2d6343c5e62c72e13111e43dc5d8c
yokota-daiki <ribrate78@gmail.com>
2023-02-10 18:19:30 +0900

    Test: Myitemモデルのmodel specを作成

--- spec/models/myitem_spec.rb
+++ spec/models/myitem_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe Myitem, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let!(:myitem) { create(:myitem) }
+  it 'user_idとitem_idが存在する場合有効である' do
+    expect(myitem).to be_valid
+  end 
+
+  it 'user_idが存在しない場合無効である' do
+    myitem.user = nil
+    expect(myitem).to be_invalid
+  end
+
+  it 'item_idが存在しない場合無効である' do
+    myitem.item = nil
+    expect(myitem).to be_invalid
+  end
 end
