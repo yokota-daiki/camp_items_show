9fd5ce083154e62aa5d9c881ed8e9ac23947b8b6
yokota-daiki <ribrate78@gmail.com>
2023-03-15 15:03:09 +0900

    func: ゲストログイン機能を追加

--- config/routes.rb
+++ config/routes.rb
@@ -24,6 +24,7 @@ Rails.application.routes.draw do
   get 'login',         to: 'user_sessions#new'
   post 'login',        to: 'user_sessions#create'
   delete 'logout',     to: 'user_sessions#destroy'
+  post 'guest_login',  to: 'user_sessions#guest_login'
   mount LetterOpenerWeb::Engine, at: '/letter_opener' if Rails.env.development?
   get '/sitemap', to: redirect("https://s3-ap-northeast-1.amazonaws.com/#{ENV['S3_BUCKET_NAME']}/sitemap.xml.gz")
 end
