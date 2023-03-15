43985d739853e6eb98ef5986d290533830911e46
yokota-daiki <ribrate78@gmail.com>
2023-03-15 15:28:18 +0900

    style: ヘッダーにゲストログイン中の判別追加

--- app/controllers/static_page_controller.rb
+++ app/controllers/static_page_controller.rb
@@ -2,6 +2,7 @@ class StaticPageController < ApplicationController
   skip_before_action :require_login, only: %i[top reload terms_of_service privacy_policy]
   def top
     @posts = Post.where(public: true).with_attached_images.sample(6)
+    user = User.find_by(params[:user_id])
   end
 
   def reload
